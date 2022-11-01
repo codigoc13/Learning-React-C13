6073cec5e81087b22372ae754caa1e9e6239d879
Steven Zuluaga Cortés <steven000069@hotmail.com>
2022-11-01 07:48:10 -0500

    Agregando titulo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Slideshow from './components/Slide/Slide.jsx'
 const App = () => {
   return (
     <div>
+      <h1>Hola mundo</h1>
       {/* <Slideshow/>
       <News/>
       <Carreras/> */}
